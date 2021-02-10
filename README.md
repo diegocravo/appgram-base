76d1bded6ffd496652f904c157b57ef7ac31106b
Diego Cravo Teixeira <cravo.diego@gmail.com>
2021-02-10 01:48:21 -0300

    first commit

--- README.md
+++ README.md
@@ -76,3 +76,4 @@ export default () => (
 ```
 
 </details>
+# appgram-base
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
-`
+`;
+
+function Title2(props) {
+  return <h1>{props.children}</h1>;
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  return <Title2>My page</Title2>;
 }
